4d9f9ea648aac1876f78ff19fb8226dc9c8484ea
FREDRAHUL0 <42122001+FREDRAHUL0@users.noreply.github.com>
2024-05-01 02:47:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"  # Specify your desired region
+}
+
+data "aws_instances" "example" {
+  instance_tags {
+    Name = "webserver"  # Replace with the name of your EC2 instance
+  }
+}
+
+output "instance_details" {
+  value = data.aws_instances.example.instances
+}
